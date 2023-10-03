6d1d81702063648b53d117317d2649afacb88621
prajitlal4 <prajit.lal4@gmail.com>
2023-10-03 11:33:17 +0800

    form fields are being set... will need to look at them again though. rough job so far.

--- src/components/FormView.jsx
+++ src/components/FormView.jsx
@@ -5,17 +5,18 @@ import { useParams } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function FormView() { //allows user to edit application, only title and description for now
-  const { instanceId } = useParams();
+  const { instanceId } = useParams(); //takes the form instance from the params
 
+  /* Sets the form instance, user object and permission string here */
   const [instance, setInstance] = useState(null);
   const [user, setUser] = useState(null);
+  const [userPermission, setUserPermission] = useState('');
 
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [salary, setSalary] = useState(0);
-
+  /* Gets all information about the application */
   const [submissionCount, setSubmissionCount] = useState(0); //setting states, maybe better to manage?
-  const [fields, setFields] = useState([]);
+  const [fieldValues, setFieldValues] = useState({});
+
+  /* useEffect Hooks */
 
   useEffect(() => { //checks to see if user is logged on before trying to load component, to stop null value from rendering
     const auth = getAuth();
@@ -25,6 +26,24 @@ function FormView() { //allows user to edit application, only title and descript
     // Clean up subscription on component unmount
     return () => unsubscribe();
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      const fetchUserPermission = async () => {
+        try {
+          const permissionDoc = doc(db, 'permissions', user.uid);
+          const permissionSnapshot = await getDoc(permissionDoc);
+  
+          if (permissionSnapshot.exists()) {
+            setUserPermission(permissionSnapshot.data().type);
+          }
+        } catch (e) {
+          console.error('Error fetching user permission:', e);
+        }
+      };
+      fetchUserPermission();
+    }
+  }, [user, db]);
 
   useEffect(() => {
     console.log(user)
@@ -37,12 +56,7 @@ function FormView() { //allows user to edit application, only title and descript
           if (instanceSnapshot.exists()) {
             const instanceData = instanceSnapshot.data();
             setInstance(instanceData);
-            setSubmissionCount(instanceData.submissionCount);
-            setFields(instanceData.fields)
-            setTitle(instanceData.title); // Set initial title and description values
-            setDescription(instanceData.description);
-            setSalary(instanceData.salary)
-
+            setFieldValues(instanceData.fields)
             console.log(instanceData)
           }
         } catch (e) {
@@ -52,6 +66,8 @@ function FormView() { //allows user to edit application, only title and descript
     fetchInstance();
     }
   }, [instanceId, user]);
+
+  /* Function Declarations */
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -64,12 +80,13 @@ function FormView() { //allows user to edit application, only title and descript
       console.error('User not authenticated');
       return;
     }
+    console.log(fieldValues);
 
     try {
       const formSubmission = await addDoc(collection(db, 'formSubmissions'), {
         instanceId,
         submissionCount: submissionCount + 1,
-        fields: fields,
+        fields: fieldValues,
         userId: user.uid
       });
       alert(`Form submitted!`);
@@ -78,7 +95,15 @@ function FormView() { //allows user to edit application, only title and descript
     }
   };
 
-  if (!instance) return <p>Loading...</p>;
+  const handleInputChange = (index, value) => { //TODO: Need to look into some way of changing / optimizing this.
+    const inputField = fieldValues[index];
+    inputField.value = value;
+    fieldValues[index] = inputField;
+    setFieldValues(fieldValues);
+    console.log(fieldValues);
+  };
+
+  if (!instance || userPermission !== "candidate") return <p>Loading...</p>;
 
   return (
     <div className="py-10 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -97,7 +122,7 @@ function FormView() { //allows user to edit application, only title and descript
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
-                    value={title} readOnly placeholder="Title"
+                    value={instance.title} readOnly placeholder="Title"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -111,7 +136,7 @@ function FormView() { //allows user to edit application, only title and descript
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <textarea
-                    value={description} readOnly placeholder="Description"
+                    value={instance.description} readOnly placeholder="Description"
                     rows={3}
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
@@ -129,7 +154,7 @@ function FormView() { //allows user to edit application, only title and descript
                   <input
                     inputMode="numeric"
                     pattern="\d*"
-                    value={salary} readOnly type="text"
+                    value={instance.salary} readOnly type="text"
                     placeholder="Enter amount"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
@@ -145,13 +170,14 @@ function FormView() { //allows user to edit application, only title and descript
           </div>
 
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
-            {instance.fields.map((field, index) => (
+            {fieldValues.map((field, index) => (
               <div className="sm:col-span-3" key={index}>
                 <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
                   {field.label}
                 </label>
                 <div className="mt-2">
                   <input
+                    onChange={(e) => handleInputChange(index, e.target.value)} // and here
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
